8426c710dd9e1b475eb1580779bdca26a8334f64
Marcin Wolicki <marcin.wolicki@gmail.com>
2016-11-19 15:46:52 +0200

    button

--- lib/blinky/blink.ex
+++ lib/blinky/blink.ex
@@ -7,18 +7,26 @@ defmodule HelloNerves.Blinky do
 
   def init([]) do
     {:ok, pidA} = Gpio.start_link(26, :output)
-    {:ok, pidB} = Gpio.start_link(16, :output)
     spawn(fn -> blink_forever(pidA) end)
-    :timer.sleep(1000)
-    spawn(fn -> blink_forever(pidB) end)
+    :timer.sleep(25)
+    {:ok, pidB} = Gpio.start_link(16, :output)
+    {:ok, pidButton} = Gpio.start_link(6, :output)
+    spawn(fn -> blink_on_buton(pidB, pidButton) end)
     {:ok, []}
+  end
+
+  def blink_on_buton(pidLed, pidButton) do
+    status = Gpio.read(pidButton)
+    Gpio.write(pidLed, status)
+    :timer.sleep(50)
+    blink_on_buton(pidLed, pidButton)
   end
 
   defp blink_forever(pid) do
     Gpio.write(pid, 1)
-    :timer.sleep(1000)
+    :timer.sleep(50)
     Gpio.write(pid, 0)
-    :timer.sleep(1000)
+    :timer.sleep(50)
     blink_forever(pid)
   end
 end
